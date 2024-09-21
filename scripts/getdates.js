94836ab262076c725bb3e98abb7809cf8b026863
Alfonso <alfonsoscudiero@gmail.com>
2024-09-20 20:59:57 -0600

    update: final review after the audit tool

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,8 @@
 // Select the DOM element for output
-const currentDate = document.querySelector("#lastModified");
+const pageLastModified = document.querySelector("#lastModified");
 
 // Set up the Date format object parameter for toLocaleString method and manipulate
-currentDate.textContent = new Date().toLocaleString();
+pageLastModified.textContent = new Date().toLocaleString();
 
-// console.log(currentDate);
+// console.log(pageLastModified);
 
