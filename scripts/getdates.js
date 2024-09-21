19c49138bd9b9efa2ccaad23f4dade7ed7c15447
Alfonso <alfonsoscudiero@gmail.com>
2024-09-20 21:18:54 -0600

    update: include current year on footer with JS

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,11 @@
 // Select the DOM element for output
+const currentCopyrightYear = document.querySelector("#currentYear");
 const pageLastModified = document.querySelector("#lastModified");
 
 // Set up the Date format object parameter for toLocaleString method and manipulate
+currentCopyrightYear.textContent = new Date().getFullYear();
 pageLastModified.textContent = new Date().toLocaleString();
 
-// console.log(pageLastModified);
+console.log(currentCopyrightYear);
+console.log(pageLastModified);
 
