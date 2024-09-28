0adc8c3f52659d974df64a70d89bc48acfe1c63e
Alfonso <alfonsoscudiero@gmail.com>
2024-09-28 15:23:16 -0600

    update: design principle assignment

---
+++ lesson02/scripts/getdate.js
@@ -0,0 +1,7 @@
+// Select the DOM element for output
+const currentCopyrightYear = document.querySelector("#currentYear");
+const pageLastModified = document.querySelector("#lastModified");
+
+// Set up the Date format object parameter for toLocaleString method and manipulate
+currentCopyrightYear.textContent = new Date().getFullYear();
+pageLastModified.textContent = new Date().toLocaleString();
