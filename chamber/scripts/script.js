a0d5469b37d14ef025cc31fecb1f95a61e225fce
Alfonso <alfonsoscudiero@gmail.com>
2024-10-11 23:16:30 -0600

    update: footer and js code added

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,6 +1,12 @@
 // Select the DOM element for output
+const yearCopyright = document.querySelector("#currentYear");
+const pageLastModified = document.querySelector("#lastModified");
 const hambutton = document.querySelector("#menu");
 const navbar = document.querySelector(".header-main-nav");
+
+// Set up the Date format object parameter for toLocaleString method and manipulate
+yearCopyright.textContent = new Date().getFullYear();
+pageLastModified.textContent = new Date().toLocaleString();
 
 // Add a click event listener to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambutton.addEventListener('click', () => {
