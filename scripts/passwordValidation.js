8d7ff29bdbaffbd243e789e9353e234efb7f77f4
Alfonso <alfonsoscudiero@gmail.com>
2024-11-06 22:30:57 -0700

    update: add password validation js

---
+++ scripts/passwordValidation.js
@@ -0,0 +1,18 @@
+//Initiate variables
+const kp1 = document.querySelector("#keyphrase");
+const kp2 = document.querySelector("#keyphrase2");
+const message = document.querySelector("#form-message");
+
+kp2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+    message.textContent = "";
+    message.classList.remove("show-message");
+
+    if (kp1.value !== kp2.value) {
+        message.textContent = "❗ Key Phrases DO NOT MATCH!";
+        message.classList.add('show-message');
+        kp2.value = "";
+        kp2.focus();
+    } 
+}
